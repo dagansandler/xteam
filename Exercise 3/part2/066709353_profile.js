d6e7cae0ab415eba064e2e964c51a1d6e5a486d4
gran33 <gran33@gmail.com>
2013-05-20 12:19:41 +0300

    part1 competed, part2 5/7 completed missing 2.1,2.2,2.7 (almost done)

---
+++ Exercise 3/part2/066709353_profile.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+	
+	//2.1
+	
+	
+	//2.2
+	
+	
+	//2.3
+	$("tr").hover(function () {
+		$(this).addClass("onRow");
+	},function () {
+		$(this).removeClass("onRow");
+	});
+	
+	//2.4
+	$("#id, #username_git").click(function () {
+		$("#id, #username_git").toggle();
+	});
+	
+	//2.5
+	$(".listInfo").click(function () {
+		$(this).children(".toExpand").toggle();
+	});
+	
+	//2.6
+	$("#profilePic").hover(function () {
+		$(this).addClass("leftTilt");
+		$(this).removeClass("rightTilt");
+		$(this).find("#image").attr("src", "066709353/images/bart_simpson.jpg");
+		$(this).find("#captionText").text("Not Today!");
+	}, function () {
+		$(this).addClass("rightTilt");
+		$(this).removeClass("leftTilt");
+		$(this).find("#image").attr("src", "066709353/images/gran33_bitstrip.jpeg");
+		$(this).find("#captionText").text("Let's Party!");
+	});
+	
+	//2.7
+	$("#footer").click(function () {
+		$(this).children("#anotherQuote").toggle();
+		setTimeout(function() {
+			$("#anotherQuote").hide();
+			$("#anotherQuoteFull").show();
+		}, 3000);
+	});
+});
+
+
+
