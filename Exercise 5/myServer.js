bbd56a539baf9bc5045e59965cdfec979ae8619d
gran33 <gran33@gmail.com>
2013-07-18 12:52:48 +0300

    The Server support only new user (registration). Redis+Socket.io doesn't work (mark as comments since error accur)

--- Exercise 5/myServer.js
+++ Exercise 5/myServer.js
@@ -1,12 +1,31 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+	console.log('open db');
+});
+
+var UsersSchema = mongoose.Schema({
+	username: String,
+	password: String,
+	firstname: String,
+	lastname: String,
+	age: Number
+});
+var User = mongoose.model('User', UsersSchema);
+
 var http = require('http');
 var fs = require('fs');
 var events = require('events');
 var server = http.createServer(requestHandler);
+var url = require('url');
 var socket = require('socket.io').listen(server),
 	_ = require('underscore')._,
 	Backbone = require('backbone'),
-	redis = require('redis'),
-	rc = redis.createClient(),
+	//redis = require('redis'),
+	//rc = redis.createClient(),
 	models = require('./Models/models');
 	
 	var root = 'Public';
@@ -32,11 +51,37 @@ function requestHandler(request, response) {
 
 server.listen(8080);
 
-
 function getHandler(request, response) {
 	console.log('GET');
 	console.log(request.url);
 	//
+	var urlContainer = url.parse(request.url, true);
+	console.log(urlContainer);
+	if(urlContainer.pathname === '/register'){
+		console.log(urlContainer.query.username);
+		console.log(urlContainer.query.pass);
+		
+		var currentUser = new User({
+			username: urlContainer.query.username,
+			password: urlContainer.query.pass,
+			firstname: urlContainer.query.firstname,
+			lastname: urlContainer.query.lastname,
+			age: urlContainer.query.age
+		});
+		
+		currentUser.save(function(err, currentUser) {
+			if (err) {
+				console.log(err);
+			} // TODO handle the error
+			else {
+				console.log('OK');
+				
+			}
+
+		});
+		response.write('xxx');
+		response.end();
+	}
      fs.exists(root + request.url, function(exists) {
          if (exists) { /*file is there*/
              fs.lstat(root + request.url, function(err, stats) {
@@ -88,3 +133,64 @@ function postHandler(request, response) {
 		
 	});
 }
+
+/*
+var kittySchema = mongoose.Schema({
+	name: String,
+	age: Number
+});
+
+
+kittySchema.methods.speak = function() {
+	var greeting = this.name ? "Meow name is " + this.name : "I don't have a name"
+	console.log(greeting);
+};
+
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var silence = new Kitten({
+	name: 'Silence',
+	age: 12
+});
+console.log(silence.name);
+silence.save(function(err, fluffy) {
+	if (err) {
+		console.log(err);
+	} // TODO handle the error
+	else {
+		console.log('OK');
+		silence.speak();
+	}
+
+});
+
+var fluffy = new Kitten({
+	name: 'fluffy',
+	age: 30
+});
+fluffy.speak(); // "Meow name is fluffy"
+
+fluffy.save(function(err, fluffy) {
+	if (err) {
+		console.log(err);
+	} // TODO handle the error
+	else {
+		console.log('OK');
+		fluffy.speak();
+	}
+
+});
+
+Kitten.find(function(err, kittens) {
+	if (err) // TODO handle err
+	console.log(kittens)
+});
+
+
+Kitten.find({
+	name: /^Fluff/
+}, function() {
+	console.log("ran")
+});
+*/
