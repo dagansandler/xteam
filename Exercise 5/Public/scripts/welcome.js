9911ea2b50d16b06acbc9a41bd73dfb81e8bce7b
Ran Greenberg <gran33@gmail.com>
2013-07-27 13:30:37 +0300

    Redis, MongoDB and NodeJS server are before enter to the backbone zone...

--- Exercise 5/Public/scripts/welcome.js
+++ Exercise 5/Public/scripts/welcome.js
@@ -1,3 +1,169 @@
 $(document).ready(function() {
-	$("#register_form").ketchup();
+    $("#register_form").ketchup();
+    $("#login_form").ketchup();
+
+    var regRequest;
+    var loginRequest;
+    $("#register_btn")
+        .on('click',function() {
+            var username = $('#reg_username').val();
+            var password = $('#reg_password').val();
+            var firstname = $('#firstname').val();
+            var lastname = $('#lastname').val();
+            var age = $('#age').val();
+
+            regRequest = $.ajax({
+                url: "/welcome.html",
+                type: "POST",
+                //contentType: "text/plain",
+                //dataType:"json",
+                data: {
+                    'action' : 'register',
+                    'username': username,
+                    'password': password,
+                    'firstname': firstname,
+                    'lastname': lastname,
+                    'age': age
+                },
+                statusCode : {
+                    200: function(response){
+                        status200Helper(response);
+                    },
+                    403: function(response){
+                        status403HelperRegister(response);
+                    }
+
+                }
+            });
+
+
+            regRequest.done(function (response, textStatus, jqXHR){
+                $("#server_answer").text('response:' + response + '\ntextStatus:' + textStatus + '\njqXHR:' + jqXHR);
+            });
+
+            regRequest.fail(function (jqXHR, textStatus, errorThrown){
+                $("#server_answer").text("fail register");
+
+                //console.log(data1)
+            });
+            return false;
+
+        });
+
+
+    $("#login_btn").on('click',function() {
+        var username = $('#login_username').val();
+        var password = $('#login_password').val();
+
+        //Sends AJAX to the nodeJS server
+        loginRequest = $.ajax({
+            url: "/welcome.html",
+            type: "POST",
+            data: {
+                'action' : 'login',
+                'username': username,
+                'password': password
+            },
+            statusCode : {
+                200: function(response){
+                    status200Helper(response);
+                },
+                403: function(response){
+                    status403HelperLogin(response);
+                }
+
+            }
+        });
+
+        loginRequest.done(function (response, textStatus, jqXHR){
+            $("#server_answer").text(response);
+
+        });
+
+        loginRequest.fail(function (jqXHR, textStatus, errorThrown){
+            $("#server_answer").text("fail login");
+
+        });
+        return false;
+    });
+
 });
+
+function status200Helper(response){
+    alert('got 200');
+    window.location.assign('/email.html');
+
+}
+
+function status403HelperRegister(response){
+    alert('got 403');
+    $("#server_answer").text("username can't be used");
+}
+
+
+function status403HelperLogin(response){
+    alert('got 403');
+    $("#server_answer").text("incorrect password");
+}
+
+// 	$('#register_form').attr('action'),
+// [{'username':$('#reg_username').val(),
+//  'password':$('#reg_password').val(),
+//  'firstname':$('#firstname').val(),
+//  'lastname':$('#lastname').val(),
+//  'age':$('#age').val()}],
+// function(jsonData) {
+// 	console.log(jsonData);
+// },
+// 'json');
+
+
+/*.submit(function(){
+ //alert($("#register_form").serialize());
+ var a = $.parseJSON($("#register_form").serialize());
+ console.log(a);
+ //alert(a.);
+ });
+ */
+
+//GIL
+// $("#login-btn").on('click', function () {
+// 		//assuming all the fields are valid!!!!!
+// 		var request;  		// variable to hold request
+// 	// capturing info of the user from  register form elements:
+// 		var username = $('#username').val();
+// 		var password = $('#password').val();
+//
+// 		// fire the request to /tests/ajaxTest
+// 		$.ajax({ url: "",
+// 					type: "POST",
+// 					data: {	'action': 'login',
+// 								'username': username,
+// 								'password':	password
+// 					},
+// 					success: function(data) {//on success
+// 								switch (data) {
+// 									case 'user doesnt exist': //the user is'nt in the db..
+// 										alert('Sorry, no such user!');
+// 									//TODO
+// 									break;
+// 									case 'bad password': //bad password!
+// 										//TODO - user was succesfuly register... what to do now?
+// 										alert('bad password!');
+// 										break;
+// 									case 'internal problem':	//something happand
+// 										//TODO -error in register. what to do?
+// 										alert('problem in server....');
+// 										break;
+// 									default: //all good!
+// 										window.location.assign('/mail.html');
+// 									break;
+// 								}
+// 					},
+// 					fail: function (jqXHR,textStatus,errorThrown){ // when the ajax call fails
+// 								// log the error to the console
+// 								console.error('The following error occured: '+textStatus,errorThrown);
+// 					}
+// 				});
+// 	});
+//
--- Exercise 5/myServer.js
+++ Exercise 5/myServer.js
@@ -1,196 +1,222 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
+var queryString = require('querystring');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
-	console.log('open db');
+    console.log('open db');
 });
 
+//Users Schema
 var UsersSchema = mongoose.Schema({
-	username: String,
-	password: String,
-	firstname: String,
-	lastname: String,
-	age: Number
+    username: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+    age: Number
+});
+
+var EmailsSchema = mongoose.Schema({
+    from: String,
+    to: String,
+    date: Date,
+    text: String
 });
 var User = mongoose.model('User', UsersSchema);
+var Email = mongoose.model('Email', EmailsSchema);
+
 
 var http = require('http');
 var fs = require('fs');
 var events = require('events');
 var server = http.createServer(requestHandler);
-var url = require('url');
+//var url = require('url');
 var socket = require('socket.io').listen(server),
-	_ = require('underscore')._,
-	Backbone = require('backbone'),
-	//redis = require('redis'),
-	//rc = redis.createClient(),
-	models = require('./Models/models');
-	
-	var root = 'Public';
+    _ = require('underscore')._,
+    Backbone = require('backbone'),
+    models = require('./Models/models'),
+    uuid = require('node-uuid');
+
+//redis
+var redis = require("redis"),
+    rc = redis.createClient();
+
+rc.on("error", function (err) {
+    console.log("Error " + err);
+});
+//
+var root = 'Public';
+var TTL = 60;
 
 function requestHandler(request, response) {
 
-	//route get/post requests
-	switch (request.method) {
-	case 'GET':
-		getHandler(request, response);
-		break;
+    //route get/post requests
+    switch (request.method) {
+        case 'GET':
+            getHandler(request, response);
+            break;
 
-	case 'POST':
-		postHandler(request, response);
-		break;
+        case 'POST':
+            postHandler(request, response);
+            break;
 
-	default:
-		break;
+        default:
+            console.log('UNHANDLE REQUEST:' + request.method);
+            break;
 
-	}
+    }
 
 }
 
 server.listen(8080);
 
 function getHandler(request, response) {
-	console.log('GET');
-	console.log(request.url);
-	//
-	var urlContainer = url.parse(request.url, true);
-	console.log(urlContainer);
-	if(urlContainer.pathname === '/register'){
-		console.log(urlContainer.query.username);
-		console.log(urlContainer.query.pass);
-		
-		var currentUser = new User({
-			username: urlContainer.query.username,
-			password: urlContainer.query.pass,
-			firstname: urlContainer.query.firstname,
-			lastname: urlContainer.query.lastname,
-			age: urlContainer.query.age
-		});
-		
-		currentUser.save(function(err, currentUser) {
-			if (err) {
-				console.log(err);
-			} // TODO handle the error
-			else {
-				console.log('OK');
-				
-			}
+    console.log('GET');
+    console.log(request.url);
+    console.log('ZIVVV:' + request.body);
 
-		});
-		response.write('xxx');
-		response.end();
-	}
-     fs.exists(root + request.url, function(exists) {
-         if (exists) { /*file is there*/
-             fs.lstat(root + request.url, function(err, stats) {
-                 if (err) {
-                     console.log("found err: " + err);
-                     throw err;
-                 }
-                 if (stats.isFile()) { /*is file*/
-                     fs.readFile(root + request.url, function(err, data) {
-                         if (err) {
-                             console.log("error on file read: " + request.url);
-                             console.log(err);
-                         }
-                         //response.headers["Connection"] = "Keep-Alive";
-                         //response.headers["Content-Type"] = content_type;
-                         response.write(data.toString());
-                         response.end();
-                         console.log("Serving file " + request.url + " to client");
-                     });
-                 } else { /*is directory*/
-                     //response.headers["Content-Type"] = "text/html";
-                     response.write("Can not access directories");
-                     response.end();
-                 }
-             });
-         } else { /*file not found*/
-             //response.status = 404;
-             //response.headers["Content-Type"] = "text/html";
-             response.write("<b>404 Not Found</b>");
-             response.end();
-         }
-     });
-	//
-	
-	
+    fs.exists(root + request.url, function(exists) {
+        if (exists) { /*file is there*/
+            fs.lstat(root + request.url, function(err, stats) {
+                if (err) {
+                    console.log("found err: " + err);
+                    throw err;
+                }
+                if (stats.isFile()) { /*is file*/
+                    fs.readFile(root + request.url, function(err, data) {
+                        if (err) {
+                            console.log("error on file read: " + request.url);
+                            console.log(err);
+                        }
+                        //response.headers["Connection"] = "Keep-Alive";
+                        //response.headers["Content-Type"] = content_type;
+                        response.write(data.toString());
+                        response.end();
+                        console.log("Serving file " + request.url + " to client");
+                    });
+                } else { /*is directory*/
+                    //response.headers["Content-Type"] = "text/html";
+                    response.write("Can not access directories");
+                    response.end();
+                }
+            });
+        } else { /*file not found*/
+            //response.status = 404;
+            //response.headers["Content-Type"] = "text/html";
+            //response.write("<b>404 Not Found</b>");
+            response.end();
+        }
+    });
 }
 
 function postHandler(request, response) {
-	console.log("POST");
-	console.log(request.url);
-	var body = '';
-	request.on('data', function(data) {
-		body += data;
-		console.log("Partial body: " + body);
-	});
-	response.on('end', function() {
-		console.log("Body: " + body);
+    console.log("POST");
+    console.log(request.url);
 
-		
-	});
+    var tmp = {ran: "greenberg"};
+    console.log(tmp);
+    console.log(tmp.ran);
+
+    //console.log('ZIVVV:'+request.body)
+    var body = '';
+    request.on('data', function(data) {
+        body += data;
+        var parseBody = queryString.parse(body);
+        console.log("Body: " + body);
+        console.log(parseBody.action);
+        switch (parseBody.action) {
+
+            case 'register':
+                console.log('REGISTER!!!');
+                var currentUser = new User({
+                    username: parseBody.username,
+                    password: parseBody.password,
+                    firstname: parseBody.firstname,
+                    lastname: parseBody.lastname,
+                    age: parseBody.age
+                });
+                //var myDocument = myCollection.findOne( { username: parseBody.username } );
+                User.findOne({username: parseBody.username}, function(err,obj) {
+                    console.log(obj);
+                    if (!obj) {
+                        var uuidTMP = startSession(parseBody.username);
+                        insertUser(currentUser, response, uuidTMP);
+                    }
+
+                    else {
+                        existsUser(parseBody, response);
+                    }
+                });
+                break;
+
+            case 'login':
+                console.log('LOGIN!!!');
+                var incomeUUID = queryString.parse(request.headers['cookie']).uuid;
+                console.log(incomeUUID);
+
+                var currentUser = new User({
+                    username: parseBody.username,
+                    password: parseBody.password
+                });
+
+                User.findOne({username: currentUser.username, password: currentUser.password}, function(err,obj) {
+                    console.log(obj);
+                    if (obj) {
+                        console.log('good :)');
+                        rc.expire(incomeUUID, TTL);
+                        response.writeHead(200, {"Content-Type": "text/plain"});
+                        response.write("OK registered");
+                        response.end();
+                    }
+                    else {
+                        console.log('bad :(');
+                        response.writeHead(403, {"Content-Type": "text/plain"});
+                        response.write("incorrect password");
+                        response.end();
+
+                    }
+                });
+
+                break;
+        };
+    });
+    response.on('end', function() {
+        console.log("End_Body: " + body);
+    });
 }
 
-/*
-var kittySchema = mongoose.Schema({
-	name: String,
-	age: Number
-});
+function insertUser (user, response, curUUID){
+    console.log("going to insert:" + user.username);
+    user.save(function(err, currentUser) {
+        if (err) {
+            console.log(err);
+        } // TODO handle the error
+        else {
+            console.log('OK '+user.username+" Addad :)");
+            var cookieToHeader = 'uuid=' + curUUID;
+            console.log(cookieToHeader);
+            response.writeHead(200, {"Content-Type": "text/plain", "Set-Cookie" : cookieToHeader});
+            response.write("user registered");
+            response.end();
 
+        }
+    });
+}
 
-kittySchema.methods.speak = function() {
-	var greeting = this.name ? "Meow name is " + this.name : "I don't have a name"
-	console.log(greeting);
-};
+function existsUser(user, response) {
 
+    response.writeHead(403, {"Content-Type": "text/plain"});
+    response.write("invalid username:" + user.username + " already exist at db");
+    response.end();
 
-var Kitten = mongoose.model('Kitten', kittySchema);
+}
 
-var silence = new Kitten({
-	name: 'Silence',
-	age: 12
-});
-console.log(silence.name);
-silence.save(function(err, fluffy) {
-	if (err) {
-		console.log(err);
-	} // TODO handle the error
-	else {
-		console.log('OK');
-		silence.speak();
-	}
+function startSession(username) {
+    var currentUUID = uuid.v1();
+    console.log("start session for:" + username + " with UUID:" + currentUUID);
+    rc.set(currentUUID, username, redis.print);
+    rc.expire(currentUUID, TTL);
+    return currentUUID;
 
-});
-
-var fluffy = new Kitten({
-	name: 'fluffy',
-	age: 30
-});
-fluffy.speak(); // "Meow name is fluffy"
-
-fluffy.save(function(err, fluffy) {
-	if (err) {
-		console.log(err);
-	} // TODO handle the error
-	else {
-		console.log('OK');
-		fluffy.speak();
-	}
-
-});
-
-Kitten.find(function(err, kittens) {
-	if (err) // TODO handle err
-	console.log(kittens)
-});
-
-
-Kitten.find({
-	name: /^Fluff/
-}, function() {
-	console.log("ran")
-});
-*/
+}
